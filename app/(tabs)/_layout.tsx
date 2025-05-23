3313621df38b7784a460389e7c74ca15ae5ccc52
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-05-23 12:54:07 +0800

    #11 Add colored calendar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,31 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="data_view"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => <AntDesign size={28} name="clockcircle" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="calendar"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="calendar" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
