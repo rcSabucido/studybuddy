a507410a9b2272eb35fa06dfed05f6037208f5f7
rcSabucido <rcsabucido@gmail.com>
2025-05-23 12:45:50 +0800

    #9 Import Popppins font to project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View>
-      <Text>Home screen</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello, world!</Text>
+        </View>
+    );
 }
 
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    text: {
+        fontSize: 30,
+        color: '#333',
+        fontFamily: 'Poppins_400Regular',
+    },
+}); 
