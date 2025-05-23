869329df35339dfb388e2c40b7b90de16d67a4b6
rcSabucido <rcsabucido@gmail.com>
2025-05-23 16:24:29 +0800

    #9 Create and import tasks component to home page

---
+++ components/Tasks.tsx
@@ -0,0 +1,41 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { XCircleIcon } from "react-native-heroicons/outline";
+
+type Props = {
+    label: string,
+    onDelete: () => void,
+};
+
+export default function Tasks({label, onDelete}: Props) {
+    return (
+        <View style={styles.buttonContainer}>
+            <Pressable style={styles.contentContainer}  onPress={() => console.log('Button pressed')}>
+                <Text style={styles.text}>{label}</Text>
+                <Pressable onPress={onDelete}>
+                    <XCircleIcon size={30} color="#fff" />
+                </Pressable>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: 390,
+        height: 87,
+        backgroundColor: '#9B41E9',
+        borderRadius: 10,
+        padding: 20,
+    },
+    contentContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    text: {
+        fontSize: 20,
+        color: '#fff',
+        fontFamily: 'Poppins_700Bold',
+    }
+})
