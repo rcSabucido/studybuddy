6ef088fe5f624ef356c17fa75309260a615b0c66
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-06-06 23:49:02 +0800

    #43 Implement data view and timer select screens for specified tasks

--- app/set_current_timer.tsx
+++ app/set_current_timer.tsx
@@ -1,22 +1,23 @@
 import BackHeader from "@/components/BackHeader";
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useState } from "react";
 import { Image, Pressable, ScrollView, Text, View } from "react-native";
 import Dropdown from 'react-native-input-select';
 import styles from './styles';
 
-function openManualTimer() {
+function openManualTimer(taskId: string | string[], taskLabel: string | string[]) {
   const router = useRouter();
-  router.push("/setup_manual_timer")
+  router.push({pathname: "/setup_manual_timer", params: {taskId, taskLabel}})
 }
 
-function openPomodoroTimer() {
+function openPomodoroTimer(taskId: string | string[], taskLabel: string | string[]) {
   const router = useRouter();
-  router.push("/pomodoro_timer")
+  router.push({pathname: "/pomodoro_timer", params: {taskId, taskLabel}})
 }
 
 export default function SetCurrentTimer() {
   const [currentWork, setCurrentWork] = useState('EI');
+  let { taskId, taskLabel } = useLocalSearchParams();
   return (
     <>
     <View
@@ -43,23 +44,50 @@ export default function SetCurrentTimer() {
             justifyContent: "center",
             alignItems: "center",
           }}>
-            <Text style={styles.header_text}>Current Work</Text>
-            <Dropdown
-              label=" "
-              placeholder="Select an option..."
-              options={[
-                { label: 'English I', value: 'EI' },
-                { label: 'Science I', value: 'SI' },
-              ]}
-              selectedValue={currentWork}
-              onValueChange={(value: any) => {setCurrentWork(value)}}
-              dropdownStyle={{
-                backgroundColor: "#D9D9D9",
-              }}
-            />
+            { 
+              taskId != null && <>
+              <Text style={styles.header_text}>Pick a timer for</Text>
+              <Text style={styles.header_text}>{taskLabel}</Text>
+              </>
+            }
+            { 
+              taskId == null && <>
+              <Text style={styles.header_text}>Current Work</Text>
+              <Dropdown
+                label=" "
+                placeholder="Select an option..."
+                options={[
+                  { label: 'English I', value: 'EI' },
+                  { label: 'Science I', value: 'SI' },
+                  { label: 'Science I', value: 'SIq' },
+                  { label: 'Science I', value: 'SI2' },
+                  { label: 'Science I', value: 'SI3' },
+                  { label: 'Science I', value: 'SI4' },
+                  { label: 'Science I', value: 'SI5' },
+                  { label: 'Science I', value: 'SI6' },
+                  { label: 'Science I', value: 'SI7' },
+                  { label: 'Science I', value: 'SI8' },
+                  { label: 'Science I', value: 'SI9' },
+                  { label: 'Science I', value: 'SI0' },
+                  { label: 'Science I', value: 'SI11' },
+                  { label: 'Science I', value: 'SI12' },
+                  { label: 'Science I', value: 'SI13' },
+                  { label: 'Science I', value: 'SI14' },
+                  { label: 'Science I', value: 'SI15' },
+                  { label: 'Science I', value: 'SI16' },
+                  { label: 'Science I', value: 'SI17' },
+                ]}
+                selectedValue={currentWork}
+                onValueChange={(value: any) => {setCurrentWork(value)}}
+                dropdownStyle={{
+                  backgroundColor: "#D9D9D9",
+                }}
+              />
+              </> 
+            }
           </View>
           <Pressable style={[styles.content_container, {backgroundColor: "#F81414"}]}
-            onPress={openPomodoroTimer} accessibilityLabel="Use pomodoro timer">
+            onPress={() => openPomodoroTimer(taskId, taskLabel)} accessibilityLabel="Use pomodoro timer">
             <Text style={[styles.container_header_text, {padding: 8}]}>Use Pomodoro Timer</Text>
             <Image
               source={require('@/assets/images/pomodoro-white.png')}
@@ -67,7 +95,7 @@ export default function SetCurrentTimer() {
             />
           </Pressable>
           <Pressable style={[styles.content_container, {marginTop: 32}]}
-            onPress={openManualTimer} accessibilityLabel="Use manual timer">
+            onPress={() => openManualTimer(taskId, taskLabel)} accessibilityLabel="Use manual timer">
             <Text style={[styles.container_header_text, {padding: 8}]}>Manually Track</Text>
             <Image
               source={require('@/assets/images/clock-white.png')}
