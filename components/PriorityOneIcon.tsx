6bba5769c23c131249f8477f496d331b77e7a099
rcSabucido <rcsabucido@gmail.com>
2025-06-06 17:57:20 +0800

    #9 Refactor priority icon texts to their right priority number

--- components/PriorityOneIcon.tsx
+++ components/PriorityOneIcon.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native"
 export default function PriorityOne() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>P0</Text>
+            <Text style={styles.text}>P1</Text>
         </View>
     )
 }
--- components/PriorityTwoIcon.tsx
+++ components/PriorityTwoIcon.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native"
 export default function PriorityTwo() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>P0</Text>
+            <Text style={styles.text}>P2</Text>
         </View>
     )
 }
