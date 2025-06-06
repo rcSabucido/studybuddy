2b19dc5dfbac9681268361376f06df84dbfba199
rcSabucido <rcsabucido@gmail.com>
2025-06-06 15:32:37 +0800

    #9 Stylize priority zero component

--- components/PriorityZeroComponent.tsx
+++ components/PriorityZeroComponent.tsx
@@ -1,4 +1,27 @@
+import { StyleSheet, Text, View } from "react-native"
 
 export default function PriorityZeroComponent() {
-    
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>P0</Text>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '19%',
+        height: 16,
+        backgroundColor: '#3E0E0E',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#F81414',
+        borderRadius: 5,
+    },
+    text: {
+        fontSize: 9,
+        color: '#fff',
+        fontFamily: 'poppins_400Regular',
+    }
+})
