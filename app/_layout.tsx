668fafeadae32f61699f9fc3d79750a3fbe13547
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-06-11 23:12:40 +0800

    #45 Implement notification for tasks with set date

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,117 @@
 import { Stack } from "expo-router";
 
+import { createClient } from "@supabase/supabase-js";
 import * as Notifications from 'expo-notifications';
 import { useEffect } from "react";
 import BackgroundService from 'react-native-background-actions';
 
 const sleep = (time: number) => new Promise<void>((resolve) => setTimeout(() => resolve(), time));
 
-const veryIntensiveTask = async (taskDataArguments: any) => {
-    await new Promise( async (resolve) => {
-        for (let i = 0; BackgroundService.isRunning(); i++) {
-            console.log(i);
-            await sleep(taskDataArguments?.delay ?? 1000);
-        }
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
+
+function getCurrentDateString() {
+  return new Date().toISOString().split('T')[0];
+}
+
+function getClippedTimeRangeAroundNow() {
+  const now = new Date();
+  const minutesBefore = 10 * 60 * 1000;
+  const minutesAfter = 10 * 60 * 1000;
+  const minTime = new Date(now.getTime() - minutesBefore);
+  const maxTime = new Date(now.getTime() + minutesAfter);
+
+  if (minTime.getHours() < 0 || now.getHours() === 0 && now.getMinutes() < 5) {
+    minTime.setHours(0, 0, 0, 0);
+  }
+
+  if (maxTime.getHours() > 23 || (now.getHours() === 23 && now.getMinutes() > 54)) {
+    maxTime.setHours(23, 59, 0, 0);
+  }
+
+  const formatTime = (date: Date) =>
+    date.toLocaleTimeString('en-GB', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
     });
+
+  return {
+    from: formatTime(minTime),
+    to: formatTime(maxTime),
+  };
+}
+
+const notifyTaskPriority = async (taskLabel: string | string[]) => {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Hi! Its your StudyBuddy.',
+      body: `${taskLabel} is due x days left, start working already!`,
+    },
+    trigger: {
+      type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+      seconds: 1,
+    },
+  });
+}
+
+const updateLastNotificationSentAt = async (
+  taskId: string | string[],
+  taskLabel: string | string[],
+) => {
+  try {
+    const { error } = await supabase
+      .from('Tasks')
+      .update({ lastNotificationSentAt: getCurrentDateString() })
+      .eq('id', taskId);
+    
+    if (error) {
+      throw error;
+    }
+
+    console.log(`Marked ${taskLabel} as done notifying.`);
+  } catch (err) {
+    console.error(`Error setting task as notified ${taskLabel}:`, err);
+  }
+}
+
+const backgroundTask = async (taskDataArguments: any) => {
+  await new Promise( async (resolve) => {
+    for (let i = 0; BackgroundService.isRunning(); i++) {
+      let timeRange = getClippedTimeRangeAroundNow();
+      let { data, error } = await supabase
+        .from('activeTasksView')
+        .select('*')
+        .gte('time', timeRange.from)
+        .lte('time', timeRange.to)
+        .neq('lastNotificationSentAt', getCurrentDateString());
+
+      data?.forEach(element => {
+        notifyTaskPriority(element.label);
+        updateLastNotificationSentAt(element.id, element.label);
+      });
+
+      await sleep(taskDataArguments?.delay ?? 60000 * 5);
+    }
+  });
 };
 
 const options = {
-    taskName: 'Example',
-    taskTitle: 'ExampleTask title',
-    taskDesc: 'ExampleTask description',
-    taskIcon: {
-        name: 'ic_launcher',
-        type: 'mipmap',
-    },
-    color: '#ff00ff',
-    linkingURI: 'studybuddy://',
-    parameters: {
-        delay: 1000,
-    },
+  taskName: 'StudyBuddy',
+  taskTitle: 'StudyBuddy',
+  taskDesc: '',
+  taskIcon: {
+    name: 'ic_launcher',
+    type: 'mipmap',
+  },
+  color: '#ff00ff',
+  linkingURI: 'studybuddy://',
+  parameters: {
+    delay: 60000 / 2,
+  },
 };
 
 Notifications.setNotificationHandler({
@@ -41,7 +125,7 @@ Notifications.setNotificationHandler({
 
 export default function RootLayout() {
   const startBackgroundService = async () => {
-    await BackgroundService.start(veryIntensiveTask, options);
+    await BackgroundService.start(backgroundTask, options);
   }
   const stopBackgroundService = async () => {
     await BackgroundService.stop();
@@ -50,7 +134,7 @@ export default function RootLayout() {
     stopBackgroundService();
     setTimeout(() => {
       startBackgroundService();
-    }, 2000);
+    }, 5000);
   }, [])
   return (
     <Stack>
