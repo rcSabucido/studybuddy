fb2016c8632410d1e80a6b8a39019933e4f81b2f
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-06-15 13:45:15 +0800

    #58 Animate back arrow button

--- components/ArrowButton.tsx
+++ components/ArrowButton.tsx
@@ -1,17 +1,33 @@
 import styles from '@/app/styles';
-import { Pressable, View } from 'react-native';
+import { Pressable, View, Animated } from 'react-native';
 import { ArrowLeftIcon } from 'react-native-heroicons/outline';
+import { useRef } from 'react';
 
 type Props = {
     onPress?: () => void;
 }
 
 export default function ArrowButton({ onPress }: Props) {
+    const scaleAnim = useRef(new Animated.Value(1)).current;
+
+    const handlePressIn = () => {
+        Animated.spring(scaleAnim, {
+            toValue: 0.75,
+            useNativeDriver: true,
+        }).start();
+    }
+
+    const handlePressOut = () => {
+        Animated.spring(scaleAnim, {
+            toValue: 1,
+            useNativeDriver: true,
+        }).start();
+    }
     return(
-        <View style={styles.button_container}>
-            <Pressable style={styles.button} onPress={onPress}>
+        <Animated.View style={[styles.button_container, { transform: [{ scale: scaleAnim }] }]}>
+            <Pressable style={styles.button} onPress={onPress} onPressIn={handlePressIn} onPressOut={handlePressOut}>
                 <ArrowLeftIcon size={25} color="black"/>
             </Pressable>
-        </View>
+        </Animated.View>
     )
 }
